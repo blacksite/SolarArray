bb2790f08ed1d06e2df9b32679c6d2da3e72782b
jlbrown5-ncat <60116362+jlbrown5-ncat@users.noreply.github.com>
2021-12-17 17:55:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,18 @@ if __name__ == '__main__':
     BROKER_URL = "3.65.154.195"
     BROKER_PORT = 1883
 
+    client = mqtt.Client()
+    client.connect(BROKER_URL, BROKER_PORT)
+
     while True:
         voltage = random.random() * 2000
 
-        client = mqtt.Client()
-        client.connect(BROKER_URL, BROKER_PORT)
-        # client.publish("topic/test", "Hello world!")
-        client.publish("solar", voltage, qos=1)
-        client.disconnect()
-        time.sleep(wait_interval)
+        try:
+            # client.publish("topic/test", "Hello world!")
+            client.publish("solar", voltage, qos=1)
+            client.disconnect()
+            time.sleep(wait_interval)
+        except Exception as e:
+            client.connect(BROKER_URL, BROKER_PORT)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
